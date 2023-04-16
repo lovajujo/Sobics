877abff17f64ebcb2a38033711d4f5613ea62f10
lovajujo <lovaszi.zsuzsanna@gmail.com>
2023-04-16 17:36:48 +0200

    grogu

--- res/script.js
+++ res/script.js
@@ -1,13 +1,14 @@
 let level=1;
+let score=0;
 let timeout=8000;
 let row_number=5;
 let column_number=6;
 let game_area;
 let ga_width;
 let ga_height;
-let cat;
-let cat_width=100;
-let cat_height;
+let grogu;
+let grogu_width=100;
+let grogu_height;
 let container;
 let curr_bricks;
 let cont_height;
@@ -21,6 +22,8 @@ let colors=[
 ];
 let brick_width;
 let brick_height;
+let id_helper=0;
+let holding=false;
 
 
 
@@ -33,28 +36,60 @@ $(document).ready(function () {
     brick_width=ga_width/column_number;
     brick_height=ga_height/13;
     cont_height=parseInt(container.css('height'));
-    cat=$('<img src="../res/cat.png" id="cat">');
-    cat.on('load', function(){
-        init_cat();
+    grogu=$('<img src="../res/groguu.png" id="grogu">');
+    grogu.on('load', function(){
+        init_grogu();
     });
-    init_ga();
+    //init_ga();
+    add_bricks(row_number);
+    $('#level').append(" "+level);
+    $('#score').append(" "+score);
+    container.on('mousemove', move_grogu);
 
-    container.on('mousemove', move_cat);
-    //TODO
-    //hover, onclick, down to cat or just move
+    $('.bricks:not(.wall)').hover(function (e){
+        if(is_clickable($(this))){
+        $(this).css({
+            opacity: 0.5
+        });
+    }},function () {
+        $(this).css({
+            opacity: 1
+        })
+    })
+    $('.bricks:not(.wall)').on('click',function (){
+        if(is_clickable($(this))){
+            pick_brick($(this));
+        }
+    });
+    container.on('click', function () {
 
+    })
     //setInterval(add_bricks, 1000);
 
 })
 
-function init_cat(){
-    cat.css({
-        width: cat_width,
-        bottom: 0,
-        left: ga_width/2-cat_width
+function place_brick(brick){
+
+}
+
+function pick_brick(brick){
+    holding=true;
+    brick.css({
+        width: brick_width/2,
+        height: brick_height/2,
+        left: brick_width/4+parseInt(brick.css('left')),
+        top: parseInt(brick.css('top'))+brick_height/4,
+    })
+}
+
+function init_grogu(){
+    grogu.css({
+        width: grogu_width,
+        bottom: 10,
+        left: ga_width/2-grogu_width
     });
-    cat_height=parseInt(cat.css('height'));
-    game_area.append(cat);
+    grogu_height=parseInt(grogu.css('height'));
+    game_area.append(grogu);
 }
 
 function init_ga(){
@@ -67,8 +102,7 @@ function init_ga(){
         })
         container.append(row);
         for(let j=0;j<column_number;j++){
-            let tile=$('<div></div>');
-            tile.addClass('tile');
+            let tile=$('<div class="tile"></div>');
             tile.css({
                 width: brick_width,
                 height: brick_height,
@@ -82,6 +116,7 @@ function init_ga(){
 }
 
 function add_bricks(rows=1){
+    id_helper++;
     for(let i=0;i<rows;i++){
         let row=$('<div></div>');
         row.css({
@@ -89,12 +124,12 @@ function add_bricks(rows=1){
             width: ga_width,
         })
         curr_bricks.css({
-            height: this.height+brick_height
+            height: $(this).height+brick_height
         })
         curr_bricks.prepend(row);
         for(let j=0;j<column_number;j++){
-            let brick=$('<div></div>');
-            brick.addClass('bricks');
+            let uid=""+id_helper+""+i+""+j;
+            let brick=$('<div id="'+uid+'" class="bricks"></div>');
             if(Math.random()<0.03){
                 brick.addClass('wall');
             }
@@ -112,19 +147,32 @@ function add_bricks(rows=1){
                 height: brick_height,
                 left: j*brick_width
             });
+            brick_array.push({
+                id: uid,
+                y:i,
+                x:j,
+                color: brick.css("background-color")
+            })
             row.append(brick);
         }
     }
+    console.log(brick_array);
 }
 
-function move_cat(ev){
+function move_grogu(ev){
     let div_pos=game_area.offset();
-    let mouse_pos_x=Math.ceil(ev.clientX-div_pos.left-cat_width/2);
-    if(mouse_pos_x>0 && mouse_pos_x<ga_width-cat_width){
-        cat.css({
+    let mouse_pos_x=Math.ceil(ev.clientX-div_pos.left-grogu_width/2);
+    if(mouse_pos_x>0 && mouse_pos_x<ga_width-grogu_width){
+        grogu.css({
             left: mouse_pos_x
         })
     }
 }
 
-
+function is_clickable(brick){
+    if(holding){
+        return false;
+    }
+    let obj=brick_array.find(o=> o.id===brick.attr('id'));
+    return !brick_array.find(o => (o.y === obj.y - 1 && o.x === obj.x));
+}
